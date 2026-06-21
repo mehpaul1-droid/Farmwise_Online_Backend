c047d20cfb486e953594355aaad444ae1240be34
Ares <ares@example.com>
2026-06-21 10:51:40 +0330

    fix BOM encoding issue

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 ﻿from fastapi import FastAPI
-=======
-from fastapi import FastAPI
->>>>>>> b53d9d90fc91fba95f2a872a0e5a58e34fe4a3b7
 
 app = FastAPI()
 
@@ -15,10 +11,6 @@ def analyze(data: dict):
     return {
         "input": data,
         "result": "AI analysis completed",
-        "profit_score": 87,
-<<<<<<< HEAD
-        "recommendation": "Use SHI protein instead of 20% soybean meal"
-=======
-        "recommendation": "Use SHI protein instead of soybean"
->>>>>>> b53d9d90fc91fba95f2a872a0e5a58e34fe4a3b7
+        "model": "SHI-Engine",
+        "profit_score": 87
     }
